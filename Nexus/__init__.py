47fc1bf7801e32b069650caaaff2a9c02b0ef781
mosetf <stanlym315@gmail.com>
2024-09-18 15:11:33 +0300

    Add Flask app factory function and register blueprints

---
+++ Nexus/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
+
+    jwt = JWTManager(app)
+
+    from .routes import routes
+
+
+
+    with app.app_context():
+        from Nexus import routes, auth, db
+        db.init_db(app)
+        app.register_blueprint(auth.auth)
+        app.register_blueprint(routes.routes)
+
+    return app
