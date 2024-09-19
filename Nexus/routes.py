f1318c8a25f6d28efe9f59e752b7506e5a266f80
mosetf <stanlym315@gmail.com>
2024-09-19 23:44:19 +0300

    Refactor routes.py to include login and signup forms

--- Nexus/routes.py
+++ Nexus/routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, url_for, flash, request
 from Nexus.auth import register, login
+from Nexus.forms import SignupForm, LoginForm
 
 routes = Blueprint('routes', __name__)
 
@@ -7,11 +8,35 @@ routes = Blueprint('routes', __name__)
 def home():
     return render_template('landing.html')
 
-@routes.route("/login", methods=["GET"])
+@routes.route("/login", methods=["GET", "POST"])
 def login_page():
-    return render_template('login.html')
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        data = {
+            'email': form.email.data,
+            'password': form.password.data
+        }
+        response = login()
+        if response.status_code == 200:
+            flash('Logged in successfully', 'success')
+            return redirect(url_for('routes.home'))
+        else:
+            flash(response.json['message'], 'danger')
+    return render_template('login.html', form=form)
 
 @routes.route("/signup", methods=["GET", "POST"])
 def signup_page():
-    return render_template('signup.html')    
+    form = SignupForm()
+    if form.validate_on_submit():
+        data = {
+            'username': form.username.data,
+            'email': form.email.data,
+            'password': form.password.data
+        }
+        response = register()
+        if response.status_code == 201:
+            flash('Account created successfully', 'success')
+            return redirect(url_for('routes.login_page'))
+        else:
+            flash(response.json['message'], 'danger')
+    return render_template('register.html', form=form)
